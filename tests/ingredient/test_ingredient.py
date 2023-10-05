2a4dcd732037b0a380f47754e7ca2a59809efa14
fabiotorres95 <engfabiotorres@outlook.com>
2023-10-05 15:54:19 -0300

    created test_ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,21 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
-# Req 1
 def test_ingredient():
-    pass
+    assert hash(Ingredient("queijo mussarela")) == hash(
+        Ingredient("queijo mussarela")
+    )
+
+    assert hash(Ingredient("queijo mussarela")) != hash(Ingredient("tomate"))
+
+    assert Ingredient("queijo mussarela") == Ingredient("queijo mussarela")
+    assert Ingredient("queijo mussarela") != Ingredient("tomate")
+
+    assert repr(
+        Ingredient("queijo mussarela")
+    ) == "Ingredient('queijo mussarela')"
+
+    assert Ingredient("queijo mussarela").name == "queijo mussarela"
+    assert Ingredient("queijo mussarela").restrictions == {
+        Restriction.LACTOSE, Restriction.ANIMAL_DERIVED
+    }
