3a711285164c16743b180710e44a2adcfb0fedfc
fabiotorres95 <engfabiotorres@outlook.com>
2023-10-05 17:02:34 -0300

    created test_dish

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,31 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+import pytest  # noqa: F401
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401
 
 
-# Req 2
 def test_dish():
-    pass
+    assert Dish('Espaguete', 1.99).name == 'Espaguete'
+
+    assert hash(Dish('Espaguete', 1.99)) == hash(Dish('Espaguete', 1.99))
+    assert hash(Dish('Espaguete', 1.99)) != hash(Dish('Strogonoff', 2.98))
+
+    assert Dish('Espaguete', 1.99) == Dish('Espaguete', 1.99)
+    assert Dish('Espaguete', 1.99) != Dish('Strogonoff', 2.98)
+
+    assert repr(Dish('Espaguete', 1.99)) == "Dish('Espaguete', R$1.99)"
+
+    with pytest.raises(TypeError):
+        Dish('Espaguete', '1.99')
+    with pytest.raises(ValueError):
+        Dish('Espaguete', 0)
+
+    new_dish = Dish('Espaguete', 1.99)
+    new_ingredient = Ingredient('manteiga')
+    new_dish.add_ingredient_dependency(new_ingredient, 1)
+
+    assert new_dish.recipe.get(new_ingredient) == 1
+    assert new_dish.get_restrictions() == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED
+    }
+    assert new_dish.get_ingredients() == {new_ingredient}
